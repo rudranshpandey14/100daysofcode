fa33d592fc406ff44c5da89cc12076b765d51104
rudranshpandey14 <pandeyrudransh16@gmail.com>
2025-11-04 16:19:53 +0530

    Add files via upload

---
+++ 09_day_Q17.c
@@ -0,0 +1,26 @@
+//Write a program to find the roots of a quadratic equation and categorize them.
+#include<stdio.h>
+int main(){
+
+    //input1
+    int i1;
+    int sum1=0;
+    for (int i1 = 1; i1 <= 5; i1++)
+    {
+        sum1+=i1;
+    }
+    printf("The sum is: %d \n" , sum1);
+
+
+    //input2
+    int i2;
+    int sum2=0;
+    for (int i2 = 1; i2 <= 10; i2++)
+    {
+        sum2+=i2;
+    }
+    printf("The sum is: %d \n" , sum2);
+    
+    
+    return 0;
+}
