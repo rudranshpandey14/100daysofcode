daca11c3c09eb1eda01e06c6d7711850bdc2dba5
rudranshpandey14 <pandeyrudransh16@gmail.com>
2025-11-04 17:04:13 +0530

    Add files via upload

---
+++ 23_day_Q46.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+/*Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+******/
+
+int main(){
+    int i;
+
+    for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+    
+    
+    
+    return 0;
+}
