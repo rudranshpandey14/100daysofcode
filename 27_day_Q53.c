3babc7987137f5c92fac750fe56f8482c8c5ad18
rudranshpandey14 <pandeyrudransh16@gmail.com>
2025-11-04 17:10:21 +0530

    Add files via upload

---
+++ 27_day_Q53.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+
+/*Q53: Write a program to print the following pattern:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+
+int main(){
+    int i;
+
+    for ( i = 1; i < 2; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 4; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 8; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 10; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+    for ( i = 1; i < 8; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+    for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+    for ( i = 1; i < 4; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+    for ( i = 1; i < 2; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+    
+    
+    
+    return 0;
+}
